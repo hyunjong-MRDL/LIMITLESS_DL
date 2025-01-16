785c175d1f5de341ecd651a55e44853cb4ecdb35
Hyunjong Kim (MR_DL) <hyunjongkim0524@gmail.com>
2025-01-16 21:11:48 +0900

    Progress

--- CT_Reconstruction/processing.py
+++ CT_Reconstruction/processing.py
@@ -1,36 +1,38 @@
 import numpy as np
 
-def window_per_slice(slice_data): # 2D data [ [x1, y1], [x2, y2], [...] ]
-    reshaped_points = np.array(slice_data).reshape(-1, 3)
-    x, y = reshaped_points[:, 0], reshaped_points[:, 1]
-    x_max, x_min = np.max(np.array(x)), np.min(np.array(x))
-    y_max, y_min = np.max(np.array(y)), np.min(np.array(y))
-    return x_max-x_min, y_max-y_min
+# def window_per_slice(slice_data): # 2D data [ [x1, y1], [x2, y2], [...] ]
+#     reshaped_points = np.array(slice_data).reshape(-1, 3)
+#     x, y = reshaped_points[:, 0], reshaped_points[:, 1]
+#     x_max, x_min = np.max(np.array(x)), np.min(np.array(x))
+#     y_max, y_min = np.max(np.array(y)), np.min(np.array(y))
+#     return x_max-x_min, y_max-y_min
 
-def total_window_list(ROI_contours):
-    slice_size = len(ROI_contours)
-    wx_list, wy_list = [], []
-    for slice in range(slice_size):
-        curr_slice = ROI_contours[slice]
-        curr_wx, curr_wy = window_per_slice(curr_slice)
-        wx_list.append(curr_wx)
-        wy_list.append(curr_wy)
-    return wx_list, wy_list
+# def total_window_list(ROI_contours):
+#     slice_size = len(ROI_contours)
+#     wx_list, wy_list = [], []
+#     for slice in range(slice_size):
+#         curr_slice = ROI_contours[slice]
+#         curr_wx, curr_wy = window_per_slice(curr_slice)
+#         wx_list.append(curr_wx)
+#         wy_list.append(curr_wy)
+#     return wx_list, wy_list
 
-def window_size(wx_list, wy_list, spacing):
-    wx_max, wx_min = np.max(np.array(wx_list)), np.min(np.array(wx_list))
-    wy_max, wy_min = np.max(np.array(wy_list)), np.min(np.array(wy_list))
-    return int(wx_max-wx_min)/spacing, int(wy_max-wy_min)/spacing
+# def window_size(wx_list, wy_list, spacing):
+#     wx_max, wx_min = np.max(np.array(wx_list)), np.min(np.array(wx_list))
+#     wy_max, wy_min = np.max(np.array(wy_list)), np.min(np.array(wy_list))
+#     return int(wx_max-wx_min)/spacing, int(wy_max-wy_min)/spacing
+
 ### Original image size: (512, 512)
 
 def per_slice_mapping(slice_data, w_size, center, spacing):
     reshaped_points = np.array(slice_data).reshape(-1, 3)
     x, y = reshaped_points[:, 0], reshaped_points[:, 1]
 
-    image = np.zeros(w_size)
+    image = np.zeros((w_size, w_size))
     for i in range(len(x)):
         x_pos, y_pos = int( (x[i] - center[0]) / spacing ), int( (y[i] - center[1]) / spacing )
-        image[x_pos, y_pos] = 1
+        px_center = int(w_size/2)
+        image[px_center+x_pos, px_center+y_pos] = 1
     return image
 
 """
